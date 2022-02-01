8eef3177f9c153e9c31c9c771d8bb5055309f907
franlodice <franlodicems@gmail.com>
2022-02-01 14:47:47 -0300

    Created NavBar Desafio02

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,30 @@
+import './NavBar.css'
+
+const NavBar = () => {
+    return (
+      <nav className='NavBar'>
+        <a href="#"><img src={"./images/epaamarillo_simple.svg"} alt="Logotipo EPA" className='logoNav zoomMenu'/></a>
+        <div>
+        <ul className='menu'>
+          <li className='categories'>
+            <a className='categorieSelected' href='#'>HOME</a>
+          </li>
+          <li className='categories zoomMenu'>
+            <a href='#snacks'>SNACKS</a>
+          </li>
+          <li className='categories zoomMenu'>
+            <a href='#bebidas'>BEBIDAS</a>
+          </li>
+          <li className='categories zoomMenu'>
+            <a href='#golosinas'>GOLOSINAS</a>
+          </li>
+        </ul>
+        </div>
+        <div className='zoomMenu'>
+        <a className ='login' href='#login'>LOGIN</a>
+        </div>
+      </nav>
+    )
+}
+
+export default NavBar
